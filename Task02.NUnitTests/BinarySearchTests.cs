aa9fe2c563e6285f9dee21822059fee68358b7d7
Artsiom <Artsiomxxx@gmail.com>
2016-07-16 21:27:53 +0300

    did the second task.

---
+++ Task02.NUnitTests/BinarySearchTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task02.BinarySearch;
+
+namespace Task02.NUnitTests
+{
+    public class BinarySearchTests
+    {
+
+        [Test, TestCaseSource(typeof(ProviderArray), nameof(ProviderArray.CasesForBinarySearchIntTest))]
+        public void BinarySearchIntTest(int[] array, int key, int expected)
+        {
+            int result = BinarySearchMethod(array, key);
+            Assert.AreEqual(expected, result);
+        }
+        [Test, TestCaseSource(typeof(ProviderArray), nameof(ProviderArray.CasesForBinarySearchDoubleTest))]
+        public void BinarySearchDoubleTest(double[] array, double key, int expected)
+        {
+            int result = BinarySearchMethod(array, key);
+            Assert.AreEqual(expected, result);
+        }
+        [Test, TestCaseSource(typeof(ProviderArray), nameof(ProviderArray.CasesForBinarySearchStringTest))]
+        public void BinarySearchStringTest(string[] array, string key, int expected)
+        {
+            int result = BinarySearchMethod(array, key);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+
+    public class ProviderArray
+    {
+        #region testcases
+        public static IEnumerable CasesForBinarySearchIntTest
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] {2, 4, 6, 8, 10},6, 2);
+                yield return new TestCaseData(new int[] { 11, 33, 432, 700, 987 },11, 0);
+                yield return new TestCaseData(new int[] { 111, 333, 444, 777, 979 },777, 3);
+                yield return new TestCaseData(new int[] { 20, 40, 50, 77, 97, 99,1000, 2000 },2000, 7);
+            }
+
+        }
+
+        public static IEnumerable CasesForBinarySearchDoubleTest
+        {
+
+            get
+            {
+                yield return new TestCaseData(new double[] { 2.3, 4.1, 6.8, 8.0, 10.1 }, 6.8, 2);
+                yield return new TestCaseData(new double[] { 11.1, 33.6, 432.7, 700.4, 987.7 }, 11.1, 0);
+                yield return new TestCaseData(new double[] { 111.3, 333, 444.2, 777.9, 979.3 }, 777.9, 3);
+                yield return new TestCaseData(new double[] { 20, 40, 50, 77, 97, 99, 1000, 2000 }, 2000.0, 7);
+            }
+
+        }
+
+        public static IEnumerable CasesForBinarySearchStringTest
+        {
+
+            get
+            {
+                yield return new TestCaseData(new string[] { "a", "b", "c", "d", "e" },"c", 2);
+                yield return new TestCaseData(new string[] { "a", "c", "e", "g", "k","l","h","r","q","x" },"r", 7);
+                yield return new TestCaseData(new string[] { "abc", "bbc", "cde", "cdo", "fbn" },"fbn",4);
+                yield return new TestCaseData(new string[] { "AAAAA", "MMM", "N", "TTTTTTTTTTTTT", "Z" },"N", 2);
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ Task02/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    public static class BinarySearch
+    {
+        public static int BinarySearchMethod<T>(T[] array, T key)
+        {
+            if (ReferenceEquals(array,null)||ReferenceEquals(key,null))
+                throw new ArgumentNullException();
+            if(array.Length == 0)
+                throw new ArgumentException();
+           return BinarySearchCalculate(array, key, 0, array.Length - 1);
+        } 
+
+        private static int BinarySearchCalculate<T>(T[] array, T key, int left, int right)
+        {
+            int middle = left + (right - left) / 2;
+            if (Comparer<T>.Default.Compare(array[middle],key) == 0)
+                return middle;
+            if (Comparer<T>.Default.Compare(array[middle], key) > 0)
+                return BinarySearchCalculate<T>(array, key, left, middle);
+            return BinarySearchCalculate<T>(array, key, middle + 1, right);
+        }
+    }
+}
